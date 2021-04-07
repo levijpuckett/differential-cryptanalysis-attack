575c5569c6c44b406c4712bc042fa1edd507839f
Levi Puckett <levijpuckett@gmail.com>
2021-04-07 14:15:25 -0700

    output probability statistics. generate a variable number of plaintext pairs for the attack.

--- differential_attack.c
+++ differential_attack.c
@@ -44,39 +44,42 @@ void generate_difference_distribution_table()
  * The plaintext difference input 0x0B00 is used. The input to the last round s-boxes will be 0x0606
  * with probability 27/1024.
  *
- * We generate 5000 plaintexts pairs satisfying the desired input differential.
+ * We generate a number of plaintexts pairs satisfying the desired input differential.
  *
  * For each pair, the ciphertext differential is partially decrypted using the target partial subkey value.
  * When the input to the last round satisfies the characteristic, the count for that target partial subkey 
  * is incremented. Every possible target subkey is tried (256 possibilities).
  *
- * differential_attack() takes an array to be populated with the counts for each target subkey, and returns
- * the key determinted to be most likely to true partial subkey.
+ * differential_attack() takes an array to be populated with the counts for each target subkey, and the 
+ * number of plaintext pairs to generate for the attack.
+ * It returns the key determinted to be most likely to true partial subkey.
  */
-uint8_t differential_attack(size_t * partial_subkey_counts)
+uint8_t differential_attack(size_t * partial_subkey_counts, size_t iterations)
 {
 	printf("Attacking cipher...\n");
-	// Input plaintext differential
+	
+	// input plaintext differential
 	uint16_t delta_p = 0x0B00;
-	for (int i = 0; i < 5000; ++i)
+
+	for (int i = 0; i < iterations; ++i)
 	{
-		// Generate a plaintext input pair
+		// generate a plaintext input pair
 		uint16_t p1 = (uint16_t) rand();
 		uint16_t p2 = p1 ^ delta_p;
 
+		// encrypt the plaintexts
 		uint16_t c1 = cipher_encrypt(p1);
 		uint16_t c2 = cipher_encrypt(p2);
-		uint16_t delta_c = c1 ^ c2;
 
 		// For every possible target partial subkey, partially decrypt ciphertext to find right/wrong pairs.
 		for (unsigned int k = 0; k < 256; ++k)
 		{
 			// only compute the input to the last round if all other s-box outputs are 0
-			if ((delta_c & 0xF0F0) == 0)
+			if (((c1 ^ c2) & 0xF0F0) == 0)
 			{
 				// split k to get the top and bottom half of the target partial subkey
+				// 0xab -> 0x0a0b to line up with the relevant s-boxes.
 				uint16_t key = ((k & (0xF << 4)) << 4) | ((k & 0xF) << 0);
-				//printf("Target partial subkey: %x\n", key);
 
 				// unmix with the partial key
 				uint16_t v1 = c1 ^ key;
@@ -86,8 +89,9 @@ uint8_t differential_attack(size_t * partial_subkey_counts)
 				uint16_t u1 = sub_inverse(v1);
 				uint16_t u2 = sub_inverse(v2);
 
-				// determine if a right pair occured
-				// if it did, increment the count for this subkey
+				// determine if a right pair occured by finding the input difference 
+				// and comparing it to the expected input difference
+				// if this is a right pair, increment the count for this subkey
 				if ((u1 ^ u2) == 0x0606) { partial_subkey_counts[k]++; }
 			}
 		}
@@ -128,16 +132,16 @@ int main(void)
 	generate_difference_distribution_table();
 
 	size_t partial_target_subkey_counts[256] = {};
-	uint8_t key = differential_attack(partial_target_subkey_counts);
+	size_t iterations = 5000;
+	uint8_t key = differential_attack(partial_target_subkey_counts, iterations);
 	printf("True partial subkey bits: (%x, %x)\n", (final_round_subkey & 0x0F00) >> 8,
 												 (final_round_subkey & 0x000F));
 	printf("Recovered key: (%x, %x) | count: %zu\n", (key & 0xF0) >> 4,
 												   (key & 0x0F), partial_target_subkey_counts[key]);
-/*
-	for (int i = 0; i < 256; ++i)
-	{
-		printf("Key: %2x | Count:%3zu\n", i, partial_target_subkey_counts[i]);
-	}
-*/
+	printf("Recovered key occured with probability %.3f\n",
+			((float)partial_target_subkey_counts[key] / (float)iterations));
+
+	printf("Expected probability is 27/1024 (%.3f)\n", 27.0 / 1024.0);
+
 	return 0;
 }
